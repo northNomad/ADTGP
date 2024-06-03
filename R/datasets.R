4066fdc645faf48e5ccf22e58c3ddbbc561aac58
Steven Chan <alexch.liu@mail.utoronto.ca>
2024-06-03 19:55:17 -0400

    rm

--- R/datasets.R
+++ R/datasets.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Raw protein data from MissionBio's 2PBMC dataset
 #'
 #' A dataset containing raw counts of 45 proteins from 3,751 cells
@@ -9,15 +8,3 @@
 #' @format A matrix with 45 columns and 3,751 rows.
 #' @source \url{https://portal.missionbio.com/datasets/2-PBMC-mix}
 "missionbio_pbmc"
-=======
-#' Raw protein data from MissionBio's 2PBMC dataset
-#'
-#' A dataset containing raw counts of 45 proteins from 3,751 cells
-#' (a mix of two PBMC samples).
-#' Sequencing was performed on an Illumina NextSeq 2000 instrument.
-#' Protein reads were generated at 339x coverage of reads/antibody/cell.
-#'
-#' @format A matrix with 45 columns and 3,751 rows.
-#' @source \url{https://portal.missionbio.com/datasets/2-PBMC-mix}
-"missionbio_pbmc"
->>>>>>> ffde5e11648c71e82dddf6686dd400f3a409ce26
